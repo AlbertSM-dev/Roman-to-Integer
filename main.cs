9e7b4e359d84132851387cc9f0a2dae079a763cb
AlbertSM-dev <71648626+AlbertSM-dev@users.noreply.github.com>
2024-03-04 18:27:17 +0000

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,5 +1,6 @@
 public class Solution {
     private readonly Dictionary<char, int> romanValue = new(){{'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}};
+    
     public int RomanToInt(string s) {
         int conversion = 0;
 
